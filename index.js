95c1106732d24326c1873b6b78820280b56ac0f0
Shantanu Gore <gore.shantanu@gmail.com>
2016-05-02 22:13:32 -0400

    removed excess try

--- index.js
+++ index.js
@@ -29,9 +29,7 @@ app.post('/webhook', function (req, res) {
 		if (event.message && event.message.text) {
 			if (!kittenMessage(event.sender.id, event.message.text)) {
 				if (!evalMessage(event.sender.id, event.message.text)) {
-					try {
-						sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-					}
+					sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
 				}
 			}
 		}

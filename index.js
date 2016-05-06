2d8429ef9f813af275e0f2ce75dbfab8aea1a84e
Shantanu Gore <gore.shantanu@gmail.com>
2016-05-06 00:17:43 -0400

    wrap message in json

--- index.js
+++ index.js
@@ -134,6 +134,28 @@ function sendStructuredMessage(recipientId) {
 	});
 };
 
+// function sendTextMessage(sender, text) {
+//   messageData = {
+//     text:text
+//   }
+//   request({
+//     url: 'https://graph.facebook.com/v2.6/me/messages',
+//     qs: {access_token:token},
+//     method: 'POST',
+//     json: {
+//       recipient: {id:sender},
+//       message: messageData,
+//     }
+//   }, function(error, response, body) {
+//     if (error) {
+//       console.log('Error sending message: ', error);
+//     } else if (response.body.error) {
+//       console.log('Error: ', response.body.error);
+//     }
+//   });
+// }
+
+
 function sendMessage(recipientId, message) {
 	console.log("in send message!");
 	console.log(message);
@@ -144,7 +166,7 @@ function sendMessage(recipientId, message) {
 		method: 'POST',
 		json: {
 			recipient: {id: recipientId},
-			message: message,
+			message: {text: message},
 		}
 	});
 
